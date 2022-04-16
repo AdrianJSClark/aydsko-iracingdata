fcbf977e248be5c131e0e6b996de7749a547322b
Adrian Clark <adrian@ayds.com.au>
2022-04-16 12:14:58 +1000

    Use TimeSpan for SubsessionChartLap Interval
    The "SubsessionChartLap" value doesn't use the normal ten-thousandth of
    a second number for the interval. The interval is a combination of two
    fields, one with a numerical value and the other with the units of that
    value. The example available to the project currently uses "ms" as the
    unit which indicates it is in "milliseconds". This code handles that and
    will convert appropriately into a "TimeSpan" on a separate property.
    
    Other units will throw an "InvalidOperationException" for now.

--- src/Aydsko.iRacingData/Results/SubsessionChartLap.cs
+++ src/Aydsko.iRacingData/Results/SubsessionChartLap.cs
@@ -1,18 +1,31 @@
 ﻿// © 2022 Adrian Clark
 // This file is licensed to you under the MIT license.
 
-using System.Text.Json.Serialization;
-
 namespace Aydsko.iRacingData.Results;
 
 public class SubsessionChartLap : SubsessionLap
 {
     [JsonPropertyName("lap_position")]
     public int LapPosition { get; set; }
+
     [JsonPropertyName("interval")]
-    public int? Interval { get; set; }
+    public long? IntervalRaw { get; set; }
+
     [JsonPropertyName("interval_units")]
     public string IntervalUnits { get; set; } = null!;
+
+    // If there's one thing iRacing is consistent about, it is inconsistency!
+    // Normally an "interval" would be in ten-thousandth's of a second (i.e. 1 minute = "600000")
+    // Here they have the "interval" and "interval_units". In all the results I've harvested, the units are "ms" (obviously "milliseconds")
+    // So we can't use the normal "JsonConverter(typeof(TenThousandthSecondDurationConverter))" on the "interval" field.
+    [JsonIgnore]
+    public TimeSpan? Interval => (IntervalRaw, IntervalUnits) switch
+    {
+        (null, _) => null,
+        (_, "ms") => (TimeSpan?)TimeSpan.FromMilliseconds((double)IntervalRaw),
+        _ => throw new NotSupportedException($"Unsupported \"IntervalUnits\" value for CustomerId \"{CustomerId}\" on lap number \"{LapNumber}\"")
+    };
+
     [JsonPropertyName("fastest_lap")]
     public bool FastestLap { get; set; }
 }
