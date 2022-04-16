bcbed9b7141eae5861b4f55a0b7c97268daf7ad5
Adrian Clark <adrian@ayds.com.au>
2022-04-16 11:38:11 +1000

    Add JSON Converter for iRacing Durations
    The raw iRacing API results use a number type which carries duration
    values to the ten-thousandth of a second. So, for example, a lap which
    was displayed in the iRacing results page as 1:23.456 would be returned
    as 834560.
    
    The conversion formula is:
    - iRacing Value => TimeSpan: TimeSpan.FromSeconds(iRacingValue / 10000D)
    - TimeSpan => iRacing Value: (long)(timeSpanValue.TotalSeconds * 10000)

---
+++ src/Aydsko.iRacingData.UnitTests/Converters/JsonConverterTestExtensions.cs
@@ -0,0 +1,69 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Aydsko.iRacingData.UnitTests.Converters;
+
+public static class JsonConverterTestExtensions
+{
+    private const string InvalidJsonObjectInputFormatMessage = "Expected input to be in the format \"{\"value\":\"(INPUT TO BE TESTED)\"}\" or \"{\"value\":(INPUT TO BE TESTED)}\" where (INPUT TO BE TESTED) was replaced with the value to be tested.";
+
+#pragma warning disable CA1062 // Validate arguments of public methods - check done with "!!" operator
+
+    public static IEnumerable<TestCaseData> ToReadValueTestCases<T>(this IEnumerable<(byte[] JsonBytes, T Value, string Name)> examples!!)
+    {
+        foreach (var (jsonValueBytes, timeValue, name) in examples)
+        {
+            yield return new TestCaseData(new object[] { jsonValueBytes }).Returns(timeValue).SetName("Read Value: " + name);
+        }
+    }
+
+    public static IEnumerable<TestCaseData> ToWriteValueTestCases<T>(this IEnumerable<(byte[] JsonBytes, T Value, string Name)> examples!!)
+    {
+        foreach (var (jsonValueBytes, timeValue, name) in examples)
+        {
+            yield return new TestCaseData(timeValue).Returns(jsonValueBytes).SetName("Write Value: " + name);
+        }
+    }
+
+    public static Utf8JsonReader ToUtf8JsonReaderForTest(this byte[] input!!)
+    {
+        var reader = new Utf8JsonReader(input.AsSpan());
+
+        if (!(reader.Read() && reader.TokenType == JsonTokenType.StartObject))
+        {
+            throw new ArgumentException(InvalidJsonObjectInputFormatMessage);
+        }
+
+        if (!(reader.Read() && reader.TokenType == JsonTokenType.PropertyName))
+        {
+            throw new ArgumentException(InvalidJsonObjectInputFormatMessage);
+        }
+
+        if (!(reader.Read() && (reader.TokenType == JsonTokenType.String || reader.TokenType == JsonTokenType.Number || reader.TokenType == JsonTokenType.Null)))
+        {
+            throw new ArgumentException(InvalidJsonObjectInputFormatMessage);
+        }
+
+        return reader;
+    }
+
+    public static byte[] WriteUsingConverter<T>(this T input, JsonConverter<T> converter!!)
+    {
+        using var outputStream = new MemoryStream();
+        using (var writer = new Utf8JsonWriter(outputStream))
+        {
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("value");
+            converter.Write(writer, input, new JsonSerializerOptions());
+            writer.WriteEndObject();
+        }
+
+        return outputStream.ToArray();
+    }
+
+#pragma warning restore CA1062 // Validate arguments of public methods
+}
---
+++ src/Aydsko.iRacingData.UnitTests/Converters/TenThousandthSecondDurationConverterTests.cs
@@ -0,0 +1,45 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+using Aydsko.iRacingData.Converters;
+using System.Text;
+using System.Text.Json;
+
+namespace Aydsko.iRacingData.UnitTests.Converters;
+public class TenThousandthSecondDurationConverterTests
+{
+    private TenThousandthSecondDurationConverter _sut = null!;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        // There is no state in the converter so it only needs to be created once for each set of test runs.
+        _sut = new TenThousandthSecondDurationConverter();
+    }
+
+    [Test, TestCaseSource(nameof(ReadValueTestCases))]
+    public TimeSpan? ReadValue(byte[] input)
+    {
+        var reader = input.ToUtf8JsonReaderForTest();
+        return _sut.Read(ref reader, typeof(TimeSpan?), new JsonSerializerOptions());
+    }
+
+    [Test, TestCaseSource(nameof(WriteValueTestCases))]
+    public byte[] WriteValue(TimeSpan? input)
+    {
+        var result = input.WriteUsingConverter(_sut);
+        Console.WriteLine(Encoding.UTF8.GetString(result));
+        return result;
+    }
+
+    public static IEnumerable<TestCaseData> ReadValueTestCases() => Examples().ToReadValueTestCases();
+    public static IEnumerable<TestCaseData> WriteValueTestCases() => Examples().ToWriteValueTestCases();
+
+    private static IEnumerable<(byte[] JsonBytes, TimeSpan? Value, string Name)> Examples()
+    {
+        yield return (Encoding.UTF8.GetBytes(@"{""value"":834560}"), new TimeSpan(0, 0, 1, 23, 456), "834560 / 1:23.456");
+        yield return (Encoding.UTF8.GetBytes(@"{""value"":0}"), TimeSpan.Zero, "0 / 0:00.000");
+        yield return (Encoding.UTF8.GetBytes(@"{""value"":null}"), null, "null / null");
+        yield return (Encoding.UTF8.GetBytes(@"{""value"":600000}"), new TimeSpan(0, 0, 1, 0, 0), "600000 / 1:00.000");
+    }
+}
