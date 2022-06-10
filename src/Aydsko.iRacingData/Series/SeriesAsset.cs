fd792cf115e8c4ce34fc5643cfc406335f4ef5af
Adrian Clark <adrian@ayds.com.au>
2022-06-11 01:19:37 +1000

    Implement Series Assets Endpoint

---
+++ src/Aydsko.iRacingData/Series/SeriesAsset.cs
@@ -0,0 +1,25 @@
+﻿namespace Aydsko.iRacingData.Series;
+
+public class SeriesAsset
+{
+    public const string ImagePathBase = "https://images-static.iracing.com/";
+
+    [JsonPropertyName("large_image")]
+    public object LargeImage { get; set; } = default!;
+
+    [JsonPropertyName("logo")]
+    public string Logo { get; set; } = default!;
+
+    [JsonPropertyName("series_copy")]
+    public string SeriesCopy { get; set; } = default!;
+
+    [JsonPropertyName("series_id")]
+    public int SeriesId { get; set; }
+
+    [JsonPropertyName("small_image")]
+    public object SmallImage { get; set; } = default!;
+}
+
+[JsonSerializable(typeof(IReadOnlyDictionary<string, SeriesAsset>)), JsonSourceGenerationOptions(WriteIndented = true)]
+internal partial class SeriesAssetReadOnlyDictionaryContext : JsonSerializerContext
+{ }
