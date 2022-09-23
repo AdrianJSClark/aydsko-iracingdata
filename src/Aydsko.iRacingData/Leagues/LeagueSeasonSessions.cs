c37c69af6d96c348871db97ba37947d34e441ef8
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2022-09-23 20:01:52 +0200

    Implements `data/league/season_sessions` endpoint

---
+++ src/Aydsko.iRacingData/Leagues/LeagueSeasonSessions.cs
@@ -0,0 +1,23 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+namespace Aydsko.iRacingData.Leagues;
+
+public class LeagueSeasonSessions
+{
+    [JsonPropertyName("sessions")]
+    public Session[] Sessions { get; set; } = Array.Empty<Session>();
+
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonPropertyName("season_id")]
+    public int SeasonId { get; set; }
+
+    [JsonPropertyName("league_id")]
+    public int LeagueId { get; set; }
+}
+
+[JsonSerializable(typeof(LeagueSeasonSessions)), JsonSourceGenerationOptions(WriteIndented = true)]
+internal partial class LeagueSeasonSessionsContext : JsonSerializerContext
+{ }
---
+++ src/Aydsko.iRacingData/Leagues/Track.cs
@@ -0,0 +1,16 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+namespace Aydsko.iRacingData.Leagues;
+
+public class Track
+{
+    [JsonPropertyName("track_id")]
+    public int TrackId { get; set; }
+
+    [JsonPropertyName("track_name")]
+    public string TrackName { get; set; } = default!;
+
+    [JsonPropertyName("config_name")]
+    public string? ConfigName { get; set; }
+}
