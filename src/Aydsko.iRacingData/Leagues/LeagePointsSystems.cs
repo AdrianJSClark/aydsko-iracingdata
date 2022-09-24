c20af5c751e7d6fa0a29e76f363bf7e181e4b50e
Adrian Clark <adrian@ayds.com.au>
2022-09-25 00:46:40 +1000

    League Points Systems Endpoint
    Fixes: #71

---
+++ src/Aydsko.iRacingData/Leagues/LeagePointsSystems.cs
@@ -0,0 +1,23 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+namespace Aydsko.iRacingData.Leagues;
+
+public class LeagePointsSystems
+{
+    [JsonPropertyName("subscribed")]
+    public bool Subscribed { get; set; }
+    
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+    
+    [JsonPropertyName("points_systems")]
+    public PointsSystem[] PointsSystems { get; set; } = default!;
+
+    [JsonPropertyName("league_id")]
+    public int LeagueId { get; set; }
+}
+
+[JsonSerializable(typeof(LeagePointsSystems)), JsonSourceGenerationOptions(WriteIndented = true)]
+internal partial class LeagePointsSystemsContext : JsonSerializerContext
+{ }
