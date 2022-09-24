0588132874dd51f3f554006d4f1f7774fdb6dfbe
Adrian Clark <adrian@ayds.com.au>
2022-09-24 23:23:40 +1000

    Implement World Record Statistics
    Fixes: #93

---
+++ src/Aydsko.iRacingData/Stats/WorldRecordsHeader.cs
@@ -0,0 +1,17 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+namespace Aydsko.iRacingData.Stats;
+
+public class WorldRecordsHeader
+{
+    [JsonPropertyName("type")]
+    public string ResultType { get; set; } = default!;
+
+    [JsonPropertyName("data")]
+    public WorldRecordsHeaderInfo Data { get; set; } = default!;
+}
+
+[JsonSerializable(typeof(WorldRecordsHeader)), JsonSourceGenerationOptions(WriteIndented = true)]
+internal partial class WorldRecordsHeaderContext : JsonSerializerContext
+{ }
---
+++ src/Aydsko.iRacingData/Stats/WorldRecordsHeaderInfo.cs
@@ -0,0 +1,22 @@
+﻿// © 2022 Adrian Clark
+// This file is licensed to you under the MIT license.
+
+namespace Aydsko.iRacingData.Stats;
+
+public class WorldRecordsHeaderInfo
+{
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonPropertyName("car_id")]
+    public int CarId { get; set; }
+
+    [JsonPropertyName("track_id")]
+    public int TrackId { get; set; }
+
+    [JsonPropertyName("chunk_info")]
+    public ChunkInfo ChunkInfo { get; set; } = default!;
+
+    [JsonPropertyName("last_updated")]
+    public DateTime LastUpdated { get; set; }
+}
