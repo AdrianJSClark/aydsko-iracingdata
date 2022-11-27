0ab20a43db210a0d1ce38ac913040a6221eff80a
Adrian Clark <adrian@ayds.com.au>
2022-11-27 17:50:06 +1000

    Add Integration Test for GetSubSessionResultAsync

---
+++ src/Aydsko.iRacingData.IntegrationTests/Results/ResultsGetTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aydsko.iRacingData.IntegrationTests.Results;
+public class ResultsGetTest : BaseIntegrationFixture
+{
+    [Test]
+    public async Task GivenAValidSubsessionIdThenAResultIsReturned()
+    {
+        var results = await Client.GetSubSessionResultAsync(50033865, true);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(results, Is.Not.Null);
+            Assert.That(results.Data, Is.Not.Null);
+        });
+    }
+}
