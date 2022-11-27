06488543564dc420bb7db5db1afaf42648149713
Adrian Clark <adrian@ayds.com.au>
2022-11-27 12:30:29 +1000

    Use "bool" For License Change Flags
    The flags that indicate if a driver's license changed during the race
    were presented as "1" and "-1". Convert that to a boolean as that makes
    more sense.

---
+++ src/Aydsko.iRacingData/Converters/BoolFromOneOrNegativeOneConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Aydsko.iRacingData.Converters;
+
+public class BoolFromOneOrNegativeOneConverter : JsonConverter<bool>
+{
+    public override bool Read(ref Utf8JsonReader reader,
+                              Type typeToConvert,
+                              JsonSerializerOptions options)
+    {
+        if (reader.TryGetInt32(out var numberValue) && (numberValue == -1 || numberValue == 1))
+        {
+            return numberValue == 1;
+        }
+
+        throw new Exception("Failed to read value.");
+    }
+
+    public override void Write(Utf8JsonWriter writer,
+                               bool value,
+                               JsonSerializerOptions options)
+    {
+        if (writer is null)
+        {
+            throw new ArgumentNullException(nameof(writer));
+        }
+
+        writer.WriteNumberValue(value ? 1 : -1);
+    }
+}
