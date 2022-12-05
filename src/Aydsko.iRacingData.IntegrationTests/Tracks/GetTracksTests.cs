c640f21ad4dc8c0b56c92da697aaedfde33f8775
Adrian Clark <adrian@ayds.com.au>
2022-12-06 00:52:19 +1000

    Document "TrackConfigLength" and Convert to km
    Include documentation about the "TrackConfigLength" and add a
    "TrackConfigLengthKm" property that converts the initial property value,
    which is in "miles", to "kilometres".

---
+++ src/Aydsko.iRacingData.IntegrationTests/Tracks/GetTracksTests.cs
@@ -0,0 +1,34 @@
+﻿using Aydsko.iRacingData.Tracks;
+
+namespace Aydsko.iRacingData.IntegrationTests.Tracks;
+
+public class GetTracksTests : BaseIntegrationFixture
+{
+    private Track[] tracksData;
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        var tracksResponse = await Client.GetTracksAsync(CancellationToken.None);
+        tracksData = tracksResponse.Data;
+    }
+
+    [TestCaseSource(nameof(GetTestCases))]
+    public decimal? CheckTrackLength(int trackId)
+    {
+        var track = tracksData.SingleOrDefault(t => t.TrackId == trackId);
+        return track?.TrackConfigLengthKm;
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCases()
+    {
+        yield return new(390) { ExpectedResult = 4.57M };
+        yield return new(391) { ExpectedResult = 3.68M };
+        yield return new(197) { ExpectedResult = 1.39M };
+        yield return new(152) { ExpectedResult = 4.44M };
+        yield return new(151) { ExpectedResult = 1.44M };
+        yield return new(95) { ExpectedResult = 5.95M };
+        yield return new(50) { ExpectedResult = 6.51M };
+        yield return new(341) { ExpectedResult = 5.89M };
+    }
+}
