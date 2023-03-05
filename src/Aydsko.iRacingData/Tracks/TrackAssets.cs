98ccb3c58afe9a0c6b70c68d347563cdbd35f845
Adrian Clark <adrian@ayds.com.au>
2023-03-05 17:11:37 +1000

    Document & Tidy "TrackAssets" Class
    - Add documentation comments.
    - Mark "GalleryImages" and "GalleryPrefix" as obsolete
    - Re-order so the "TrackId" property is at the top (this makes sense in
      my head)

--- src/Aydsko.iRacingData/Tracks/TrackAssets.cs
+++ src/Aydsko.iRacingData/Tracks/TrackAssets.cs
@@ -3,57 +3,87 @@
 
 namespace Aydsko.iRacingData.Tracks;
 
+/// <summary>Content related to a track, including images.</summary>
 public class TrackAssets
 {
     public const string ImagePathBase = "https://images-static.iracing.com/";
 
+    /// <summary>Unique identifier for the track this relates to.</summary>
+    [JsonPropertyName("track_id")]
+    public int TrackId { get; set; }
+
+    /// <summary>Latitude and longitude of the circuit.</summary>
     [JsonPropertyName("coordinates")]
     public string? Coordinates { get; set; }
 
     [JsonPropertyName("detail_copy")]
     public string? DetailCopy { get; set; }
 
+    /// <summary>Additional technical detail about the track in HTML format.</summary>
     [JsonPropertyName("detail_techspecs_copy")]
-    public object? DetailTechspecsCopy { get; set; }
+    public string? DetailTechnicalSpecificationsCopy { get; set; }
+
+    [Obsolete("Use DetailTechnicalSpecificationsCopy instead.")]
+    public object? DetailTechspecsCopy { get => DetailTechnicalSpecificationsCopy; set => DetailTechnicalSpecificationsCopy = value?.ToString(); }
 
     [JsonPropertyName("detail_video")]
     public object? DetailVideo { get; set; }
 
+    /// <summary>Folder name within the static images site where the large and small track images are stored.</summary>
     [JsonPropertyName("folder")]
     public string? Folder { get; set; }
 
-    [JsonPropertyName("gallery_images")]
+    [JsonPropertyName("gallery_images"), Obsolete("Property is a legacy item that does not represent how things work. (See: https://forums.iracing.com/discussion/comment/310714/#Comment_310714)")]
     public string? GalleryImages { get; set; }
 
-    [JsonPropertyName("gallery_prefix")]
+    [JsonPropertyName("gallery_prefix"), Obsolete("Property is a legacy item that does not represent how things work. (See: https://forums.iracing.com/discussion/comment/310714/#Comment_310714)")]
     public string? GalleryPrefix { get; set; }
 
+    /// <summary>File name for the blurred image of the track, in a large size.</summary>
+    /// <remarks>
+    /// <para>Needs to be combined with the <see cref="ImagePathBase"/> and <see cref="Folder"/> to build a full URL.</para>
+    /// <para>Pre-built URL available in the <see cref="LargeImageUrl"/> property.</para>
+    /// </remarks>
     [JsonPropertyName("large_image")]
     public string? LargeImage { get; set; }
 
+    /// <summary>URL to a blurred image of the track, in a large size.</summary>
+    /// <remarks>Pre-built URL of the value in the <see cref="LargeImage"/> property.</remarks>
     [JsonIgnore]
     public Uri LargeImageUrl => new(new(new(ImagePathBase), Folder?.TrimEnd('/') + '/'), LargeImage);
 
+    /// <summary>Logo image for the track.</summary>
+    /// <remarks>
+    /// <para>Needs to be combined with the <see cref="ImagePathBase"/>  to build a full URL.</para>
+    /// <para>Pre-built URL available in the <see cref="LogoUrl"/> property.</para>
+    /// </remarks>
     [JsonPropertyName("logo")]
     public string? Logo { get; set; }
 
+    /// <summary>URL to a logo image for the track.</summary>
+    /// <remarks>Pre-built URL of the value in the <see cref="Logo"/> property.</remarks>
     [JsonIgnore]
     public Uri LogoUrl => new(new(ImagePathBase), Logo);
 
     [JsonPropertyName("north")]
     public string? North { get; set; }
 
+    /// <summary>Total number of screenshot images available.</summary>
     [JsonPropertyName("num_svg_images")]
     public int NumberOfSvgImages { get; set; }
 
+    /// <summary>File name for the blurred image of the track, in a small size.</summary>
+    /// <remarks>
+    /// <para>Needs to be combined with the <see cref="ImagePathBase"/> and <see cref="Folder"/> to build a full URL.</para>
+    /// <para>Pre-built URL available in the <see cref="SmallImageUrl"/> property.</para>
+    /// </remarks>
     [JsonPropertyName("small_image")]
     public string? SmallImage { get; set; }
 
+    /// <summary>URL to a blurred image of the track, in a small size.</summary>
+    /// <remarks>Pre-built URL of the value in the <see cref="SmallImage"/> property.</remarks>
     [JsonIgnore]
     public Uri SmallImageUrl => new(new(new(ImagePathBase), Folder?.TrimEnd('/') + '/'), SmallImage);
-
-    [JsonPropertyName("track_id")]
-    public int TrackId { get; set; }
 
     /// <summary>The base URL for the track map images detailed in <see cref="TrackMapLayers"/>.</summary>
     /// <seealso cref="MapLayers" />
