c474486a2be4d7f675a56d7d7e8bf242e0903eff
Adrian Clark <adrian@ayds.com.au>
2023-04-07 23:05:44 +1000

    Include Additional Documentation

--- src/Aydsko.iRacingData/LoginResponse.cs
+++ src/Aydsko.iRacingData/LoginResponse.cs
@@ -2,10 +2,10 @@
 // This file is licensed to you under the MIT license.
 
 using Aydsko.iRacingData.Converters;
-using Aydsko.iRacingData.Lookups;
 
 namespace Aydsko.iRacingData;
 
+/// <summary>Result of a login request.</summary>
 public class LoginResponse
 {
     [JsonPropertyName("authcode"), JsonConverter(typeof(StringFromStringOrNumberConverter))]
@@ -17,9 +17,12 @@ public class LoginResponse
     [JsonPropertyName("autoLoginToken")]
     public object? AutoLoginToken { get; set; }
 
+    /// <summary>Unique identifier of the customer's account.</summary>
+    /// <remarks>Contains <see langword="null"/> if the authentication was not successful.</remarks>
     [JsonPropertyName("custId")]
     public int? CustomerId { get; set; }
 
+    /// <summary>Customer's email address.</summary>
     [JsonPropertyName("email")]
     public string? Email { get; set; }
 
@@ -38,6 +41,8 @@ public class LoginResponse
     [JsonPropertyName("inactive")]
     public bool? IsInactive { get; set; }
 
+    /// <summary>Indicates that CAPTCHA verification is required.</summary>
+    /// <remarks>Authentication from this IP Address will continue to fail until the user authenticates with a browser and completes the CAPTCHA.</remarks>
     [JsonPropertyName("verificationRequired")]
     public bool? VerificationRequired { get; set; }
 
