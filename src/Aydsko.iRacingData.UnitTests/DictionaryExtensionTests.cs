b029815969a403d12bb23f0a4d55f20ef034ff44
Adrian Clark <adrian@ayds.com.au>
2023-08-16 22:54:32 +1000

    Resolve Test Issues

--- src/Aydsko.iRacingData.UnitTests/DictionaryExtensionTests.cs
+++ src/Aydsko.iRacingData.UnitTests/DictionaryExtensionTests.cs
@@ -13,12 +13,12 @@ public class DictionaryExtensionTests
 #pragma warning disable CA1861 // Avoid constant arrays as arguments - not worth it for these unit test cases.
     public static IEnumerable<TestCaseData> GetTestCases()
     {
-        yield return new TestCaseData(new ExampleData<string>("foo")).Returns(new KeyValuePair<string, object?>("value", "foo"));
-        yield return new TestCaseData(new ExampleData<DateTime>(new DateTime(2023, 4, 22, 11, 12, 13))).Returns(new KeyValuePair<string, object?>("value", new DateTime(2023, 4, 22, 11, 12, 13)));
-        yield return new TestCaseData(new ExampleData<int[]>(new[] { 1, 2, 3 })).Returns(new KeyValuePair<string, object?>("value", new[] { 1, 2, 3 }));
-        yield return new TestCaseData(new ExampleData<IEnumerable<string>>(new string[] { "a", "b", "c" }.AsEnumerable())).Returns(new KeyValuePair<string, object?>("value", new string[] { "a", "b", "c" }.AsEnumerable()));
-        yield return new TestCaseData(new ExampleData<bool>(true)).Returns(new KeyValuePair<string, object?>("value", true));
-        yield return new TestCaseData(new ExampleData<Common.EventType>(Common.EventType.Practice)).Returns(new KeyValuePair<string, object?>("value", Common.EventType.Practice));
+        yield return new TestCaseData(new ExampleData<string>("foo")).Returns(new KeyValuePair<string, object?>("Value", "foo"));
+        yield return new TestCaseData(new ExampleData<DateTime>(new DateTime(2023, 4, 22, 11, 12, 13))).Returns(new KeyValuePair<string, object?>("Value", new DateTime(2023, 4, 22, 11, 12, 13)));
+        yield return new TestCaseData(new ExampleData<int[]>(new[] { 1, 2, 3 })).Returns(new KeyValuePair<string, object?>("Value", new[] { 1, 2, 3 }));
+        yield return new TestCaseData(new ExampleData<IEnumerable<string>>(new string[] { "a", "b", "c" }.AsEnumerable())).Returns(new KeyValuePair<string, object?>("Value", new string[] { "a", "b", "c" }.AsEnumerable()));
+        yield return new TestCaseData(new ExampleData<bool>(true)).Returns(new KeyValuePair<string, object?>("Value", true));
+        yield return new TestCaseData(new ExampleData<Common.EventType>(Common.EventType.Practice)).Returns(new KeyValuePair<string, object?>("Value", Common.EventType.Practice));
     }
 #pragma warning restore CA1861 // Avoid constant arrays as arguments
 }
