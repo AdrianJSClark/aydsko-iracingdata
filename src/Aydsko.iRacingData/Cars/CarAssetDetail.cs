c1261f86a04cef2501ed13447e83bcc3c62a1c1c
Adrian Clark <adrian@ayds.com.au>
2024-08-04 22:46:34 +1000

    CarAssetDetail URL Improvements & Documentation

--- src/Aydsko.iRacingData/Cars/CarAssetDetail.cs
+++ src/Aydsko.iRacingData/Cars/CarAssetDetail.cs
@@ -39,29 +39,57 @@ public class CarAssetDetail
     [JsonPropertyName("group_name")]
     public object? GroupName { get; set; }
 
+    /// <summary>File name for the larger image of the vehicle.</summary>
+    /// <remarks>Must be combined with <see cref="ImagePathBase"/> and <see cref="Folder"/> to be useful.</remarks>
     [JsonPropertyName("large_image")]
     public string LargeImage { get; set; } = default!;
 
+    /// <summary>The full URL to the vehicle's large image.</summary>
+    /// <seealso cref="LargeImage"/>
     [JsonIgnore]
     public Uri LargeImageUri => new(string.Join("/", [ImagePathBase.Trim('/'), Folder.Trim('/'), LargeImage.Trim('/')]));
 
+    /// <summary>File name for the logo image of the vehicle's manufacturer.</summary>
+    /// <remarks>Must be combined with <see cref="ImagePathBase"/> to be useful.</remarks>
     [JsonPropertyName("logo")]
     public string Logo { get; set; } = default!;
 
+    /// <summary>The full URL to the image containing the manufacturer logo.</summary>
+    /// <seealso cref="Logo"/>
     [JsonIgnore]
     public Uri LogoUri => new(string.Join("/", [ImagePathBase.Trim('/'), Logo.Trim('/')]));
 
+    /// <summary>File name for the smaller image of the vehicle.</summary>
+    /// <remarks>Must be combined with <see cref="ImagePathBase"/> and <see cref="Folder"/> to be useful.</remarks>
     [JsonPropertyName("small_image")]
     public string SmallImage { get; set; } = default!;
 
+    /// <summary>The full URL to the vehicle's small image.</summary>
+    /// <seealso cref="SmallImage"/>
     [JsonIgnore]
     public Uri SmallImageUri => new(string.Join("/", [ImagePathBase.Trim('/'), Folder.Trim('/'), SmallImage.Trim('/')]));
 
+    /// <summary>File name for the logo image of the vehicle's series sponsor, if available.</summary>
+    /// <remarks>Must be combined with <see cref="ImagePathBase"/> to be useful.</remarks>
     [JsonPropertyName("sponsor_logo")]
-    public object? SponsorLogo { get; set; }
+    public string? SponsorLogo { get; set; }
 
+    /// <summary>The full URL for the logo image of the vehicle's series sponsor, if available.</summary>
+    /// <seealso cref="SponsorLogo"/>
+    [JsonIgnore]
+    public Uri? SponsorLogoUri => SponsorLogo is string { Length: > 0 }
+                                  ? new(string.Join("/", [ImagePathBase.Trim('/'), SponsorLogo.Trim('/')]))
+                                  : null;
+
+    /// <summary>Relative path of the vehicle paint template, if available.</summary>
     [JsonPropertyName("template_path")]
-    public string TemplatePath { get; set; } = default!;
+    public string? TemplatePath { get; set; } = default!;
+
+    /// <summary>The full URL to the vehicle's paint template, if available.</summary>
+    [JsonIgnore]
+    public Uri? TemplateUri => TemplatePath is string { Length: > 0 }
+                                ? new($"https://ir-core-sites.iracing.com/members/member_images/cars/{TemplatePath.Trim('/')}")
+                                : null;
 }
 
 [JsonSerializable(typeof(IReadOnlyDictionary<string, CarAssetDetail>)), JsonSourceGenerationOptions(WriteIndented = true)]
