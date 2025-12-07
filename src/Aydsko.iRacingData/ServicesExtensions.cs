d506d3412bbe806a5e8ed59b854333efed785ba8
Adrian Clark <adrian@ayds.com.au>
2025-12-07 15:04:25 +1000

    WIP - Not Working

---
+++ src/Aydsko.iRacingData/ServicesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Aydsko.iRacingData;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection AddIRacingData(this IServiceCollection services, string configurationPath = "iRacing")
+    {
+        services.AddOptions<iRacingOptions>()
+                .BindConfiguration(configurationPath)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+        return services;
+    }
+}
---
+++ src/Aydsko.iRacingData/iRacingOptions.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aydsko.iRacingData;
+
+#if NET6_0_OR_GREATER
+
+public class iRacingOptions
+{
+    /// <summary>iRacing user name to use for authentication.</summary>
+    [Required]
+    public required string Username { get; set; }
+
+    /// <summary>Password associated with the iRacing user name used to authenticate.</summary>
+    [Required]
+    public required string Password { get; set; }
+
+    /// <summary>The <c>client_id</c> value for <c>password_limited</c> OAuth flow.</summary>
+    /// <seealso href="https://oauth.iracing.com/oauth2/book/token_endpoint.html#password-limited-grant" />
+    [Required]
+    public required string ClientId { get; set; }
+
+    /// <summary>The <c>client_secret</c> value for <c>password_limited</c> OAuth flow.</summary>
+    /// <seealso href="https://oauth.iracing.com/oauth2/book/token_endpoint.html#password-limited-grant" />
+    [Required]
+    public required string ClientSecret { get; set; }
+}
+
+#else
+
+public class iRacingOptions
+{
+    /// <summary>iRacing user name to use for authentication.</summary>
+    [Required]
+    public string Username { get; set; } = default!;
+
+    /// <summary>Password associated with the iRacing user name used to authenticate.</summary>
+    [Required]
+    public string Password { get; set; } = default!;
+
+    /// <summary>The <c>client_id</c> value for <c>password_limited</c> OAuth flow.</summary>
+    /// <seealso href="https://oauth.iracing.com/oauth2/book/token_endpoint.html#password-limited-grant" />
+    [Required]
+    public string ClientId { get; set; } = default!;
+
+    /// <summary>The <c>client_secret</c> value for <c>password_limited</c> OAuth flow.</summary>
+    /// <seealso href="https://oauth.iracing.com/oauth2/book/token_endpoint.html#password-limited-grant" />
+    [Required]
+    public string ClientSecret { get; set; } = default!;
+}
+
+#endif
