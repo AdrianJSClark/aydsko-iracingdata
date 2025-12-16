8f2529db89425f6ecd16857c8bc06756d7524bc2
Adrian Clark <adrian@ayds.com.au>
2025-12-16 23:49:11 +1000

    Add Test CLI Project

---
+++ src/Aydsko.iRacingData.TestCli/Program.cs
@@ -0,0 +1,18 @@
+﻿using Aydsko.iRacingData;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>(true)
+    .Build();
+
+var services = new ServiceCollection();
+services.AddSingleton<IConfiguration>(configuration);
+
+//services.AddIRacingDataApi()
+
+using var serviceProvider = services.BuildServiceProvider();
+
+
