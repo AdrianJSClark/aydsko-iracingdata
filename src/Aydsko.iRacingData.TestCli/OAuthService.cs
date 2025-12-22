1b76100b8216af31e5b36548f9cd57a63a14ca98
Adrian Clark <adrian@ayds.com.au>
2025-12-22 13:07:20 +1000

    Implement Refresh Token Retrieval

--- src/Aydsko.iRacingData.TestCli/OAuthService.cs
+++ src/Aydsko.iRacingData.TestCli/OAuthService.cs
@@ -20,11 +20,28 @@ internal class OAuthService(HttpClient httpClient, IConfiguration configuration,
         if (configuration["iRacing:ClientSecret"] is not string clientSecret) throw new Exception("Missing configuration value \"iRacing:ClientSecret\".");
         if (configuration["iRacing:RedirectUrl"] is not string redirectUrl) throw new Exception("Missing configuration value \"iRacing:RedirectUrl\".");
 
+        var baseAddress = httpClient.BaseAddress ?? new("https://oauth.iracing.com");
+
         var savedToken = await TokenStorageService.ReadTokenSaveDataAsync(cancellationToken);
 
-        if (savedToken != null && timeProvider.GetUtcNow() < savedToken.TokenExpiryInstant)
+        if (savedToken != null)
         {
-            return new(savedToken.TokenResponse.AccessToken, savedToken.TokenExpiryInstant);
+            var now = timeProvider.GetUtcNow();
+            if (now < savedToken.TokenExpiryInstant)
+            {
+                return new(savedToken.TokenResponse.AccessToken, savedToken.TokenExpiryInstant);
+            }
+            else if (now < savedToken.RefreshExpiryInstant)
+            {
+                var refreshedTokenData = await RequestRefreshTokenGrantAsync(clientId, clientSecret, savedToken.TokenResponse.RefreshToken, baseAddress, cancellationToken);
+                await TokenStorageService.WriteTokenSaveDataAsync(refreshedTokenData, cancellationToken);
+
+                Console.WriteLine("--------------- REFRESHED TOKEN START ---------------");
+                Console.WriteLine(JsonSerializer.Serialize(refreshedTokenData, JsonSerializerOptions.Web));
+                Console.WriteLine("---------------- REFRESHED TOKEN END ----------------");
+
+                return new(refreshedTokenData.TokenResponse.AccessToken, refreshedTokenData.TokenExpiryInstant);
+            }
         }
 
         var pkceCodeVerifier = Base64Url.EncodeToString(RandomNumberGenerator.GetBytes(32));
@@ -40,7 +57,6 @@ internal class OAuthService(HttpClient httpClient, IConfiguration configuration,
             ["scope"] = "iracing.auth iracing.profile",
         };
 
-        var baseAddress = httpClient.BaseAddress ?? new("https://oauth.iracing.com/oauth2");
         var authorizeUrl = new Uri(baseAddress, new Uri("/oauth2/authorize", UriKind.Relative)).ToUrlWithQuery(authorizeParameters);
 
         Console.WriteLine(authorizeUrl.AbsoluteUri);
@@ -66,6 +82,47 @@ internal class OAuthService(HttpClient httpClient, IConfiguration configuration,
             throw new Exception("Missing \"code\" in response from iRacing Authentication.");
         }
 
+        var tokenData = await RequestAuthorizationCodeGrantAsync(clientId, clientSecret, code, redirectUrl, pkceCodeVerifier, baseAddress, cancellationToken);
+
+        await TokenStorageService.WriteTokenSaveDataAsync(tokenData, cancellationToken);
+
+        Console.WriteLine("-------------------- TOKEN START --------------------");
+        Console.WriteLine(JsonSerializer.Serialize(tokenData, JsonSerializerOptions.Web));
+        Console.WriteLine("--------------------  TOKEN END  --------------------");
+
+        context.Response.StatusCode = 200;
+        context.Response.StatusDescription = "OK";
+        var response = Encoding.UTF8.GetBytes($"<html><head><title>{typeof(Program).Assembly.GetName().Name} ({typeof(Program).Assembly.GetName().Version})</title><body><h1>Done! Please close this window.</h1></body></html>");
+        context.Response.ContentLength64 = response.Length;
+        await context.Response.OutputStream.WriteAsync(response, cancellationToken);
+        context.Response.OutputStream.Close();
+        context.Response.Close();
+
+        listener.Close();
+
+        return new(tokenData.TokenResponse.AccessToken, timeProvider.GetUtcNow().AddSeconds(tokenData.TokenResponse.ExpiresInSeconds));
+    }
+
+    internal static string EncodePassword(string username, string password)
+    {
+#pragma warning disable CA1308 // Normalize strings to uppercase - iRacing API requires lowercase
+        var passwordAndEmail = password + (username?.ToLowerInvariant());
+#pragma warning restore CA1308 // Normalize strings to uppercase
+
+        var hashedPasswordAndEmailBytes = SHA256.HashData(Encoding.UTF8.GetBytes(passwordAndEmail));
+        var encodedHash = Convert.ToBase64String(hashedPasswordAndEmailBytes);
+        return encodedHash;
+    }
+#pragma warning restore CA2201 // Do not raise reserved exception types
+
+    private async Task<TokenSaveData> RequestAuthorizationCodeGrantAsync(string clientId,
+                                                                         string clientSecret,
+                                                                         string code,
+                                                                         string redirectUrl,
+                                                                         string pkceCodeVerifier,
+                                                                         Uri baseAddress,
+                                                                         CancellationToken cancellationToken = default)
+    {
         var tokenParameters = new Dictionary<string, string>
         {
             ["grant_type"] = "authorization_code",
@@ -76,6 +133,28 @@ internal class OAuthService(HttpClient httpClient, IConfiguration configuration,
             ["code_verifier"] = pkceCodeVerifier,
         };
 
+        return await RequestTokenAsync(baseAddress, tokenParameters, cancellationToken);
+    }
+
+    private async Task<TokenSaveData> RequestRefreshTokenGrantAsync(string clientId,
+                                                                         string clientSecret,
+                                                                         string refreshToken,
+                                                                         Uri baseAddress,
+                                                                         CancellationToken cancellationToken = default)
+    {
+        var tokenParameters = new Dictionary<string, string>
+        {
+            ["grant_type"] = "refresh_token",
+            ["client_id"] = clientId,
+            ["client_secret"] = EncodePassword(clientId, clientSecret),
+            ["refresh_token"] = refreshToken,
+        };
+
+        return await RequestTokenAsync(baseAddress, tokenParameters, cancellationToken);
+    }
+
+    private async Task<TokenSaveData> RequestTokenAsync(Uri baseAddress, Dictionary<string, string> tokenParameters, CancellationToken cancellationToken)
+    {
         // Doesn't matter if this is a bit early. That just gives us some buffer when we calculate the token lifetimes.
         var utcNow = timeProvider.GetUtcNow();
 
@@ -101,36 +180,8 @@ internal class OAuthService(HttpClient httpClient, IConfiguration configuration,
             refreshExpiryInstant = utcNow.AddSeconds(refreshExpiryInSeconds);
         }
 
-        await TokenStorageService.WriteTokenSaveDataAsync(new(tokenDetail, tokenExpiryInstant, refreshExpiryInstant), cancellationToken);
-
-        Console.WriteLine("-------------------- TOKEN START --------------------");
-        Console.WriteLine(JsonSerializer.Serialize(tokenDetail, JsonSerializerOptions.Web));
-        Console.WriteLine("--------------------  TOKEN END  --------------------");
-
-        context.Response.StatusCode = 200;
-        context.Response.StatusDescription = "OK";
-        var response = Encoding.UTF8.GetBytes($"<html><head><title>{typeof(Program).Assembly.GetName().Name} ({typeof(Program).Assembly.GetName().Version})</title><body><h1>Done! Please close this window.</h1></body></html>");
-        context.Response.ContentLength64 = response.Length;
-        await context.Response.OutputStream.WriteAsync(response, cancellationToken);
-        context.Response.OutputStream.Close();
-        context.Response.Close();
-
-        listener.Close();
-
-        return new(tokenDetail.AccessToken, timeProvider.GetUtcNow().AddSeconds(tokenDetail.ExpiresInSeconds));
+        return new(tokenDetail, tokenExpiryInstant, refreshExpiryInstant);
     }
-
-    internal static string EncodePassword(string username, string password)
-    {
-#pragma warning disable CA1308 // Normalize strings to uppercase - iRacing API requires lowercase
-        var passwordAndEmail = password + (username?.ToLowerInvariant());
-#pragma warning restore CA1308 // Normalize strings to uppercase
-
-        var hashedPasswordAndEmailBytes = SHA256.HashData(Encoding.UTF8.GetBytes(passwordAndEmail));
-        var encodedHash = Convert.ToBase64String(hashedPasswordAndEmailBytes);
-        return encodedHash;
-    }
-#pragma warning restore CA2201 // Do not raise reserved exception types
 }
 
 internal record TokenSaveData(OAuthTokenResponse TokenResponse, DateTimeOffset TokenExpiryInstant, DateTimeOffset? RefreshExpiryInstant);
